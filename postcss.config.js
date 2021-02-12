373db72ffb1b2fbe95591bd58c11522b84a2431f
Daniel Atwood <daniel@atwood.io>
2021-02-12 00:54:37 -0500

    Purging css

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
-module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {}
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: ['./hugo_stats.json'],
+    defaultExtractor: (content) => {
+        let els = JSON.parse(content).htmlElements;
+        return els.tags.concat(els.classes, els.ids);
     }
-}
+});
+
+module.exports = {
+    plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+        ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
+    ]
+};
