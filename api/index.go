78e2e673278e1f3378dc90fd76daadbe557bccac
Daniel Atwood <daniel@atwood.io>
2021-02-17 19:13:19 -0500

    Added hello-world function

---
+++ api/index.go
@@ -0,0 +1,10 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module atwood.io/atwood.io
+
+go 1.15
