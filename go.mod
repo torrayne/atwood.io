6779473531fe396c0fe4280e4f4650a8d0a83559
Daniel Atwood <daniel@atwood.io>
2021-03-07 21:13:08 -0500

    Update Formailer

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/djatwood/atwood.io
 go 1.15
 
 require (
-	github.com/aws/aws-lambda-go v1.22.0
-	github.com/djatwood/formailer v0.3.3
+	github.com/aws/aws-lambda-go v1.23.0
+	github.com/djatwood/formailer v0.4.0
 )
---
+++ main.go
@@ -0,0 +1,18 @@
+package api
+
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/djatwood/formailer"
+	"github.com/djatwood/formailer/handlers"
+)
+
+func main() {
+	f := make(formailer.Forms)
+	f.Add("Contact", formailer.Email{
+		To:      "daniel@atwood.io",
+		From:    `"Atwood.io" <daniel@atwood.io>`,
+		Subject: "New Contact Form Submission",
+	})
+
+	lambda.Start(handlers.Netlify(f))
+}
